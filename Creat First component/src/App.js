9513f56d7b06e6c0b0c5e363d3c59791566d2291
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-10-27 22:19:51 +0200

    Add files via upload

---
+++ Creat First component/src/App.js
@@ -0,0 +1,15 @@
+	// src/App.js
+	
+	import React from "react";
+	import HelloWorld from "./HelloWorld";
+	
+	function App() {
+	return (
+	<div className="App">
+	<HelloWorld />
+	</div>
+	);
+	}
+	
+	export default App;
+	
